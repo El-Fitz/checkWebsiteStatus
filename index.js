777fadb05c6471a929295ac38fcf5f1193418dbf
Thomas Léger <El-Fitz@users.noreply.github.com>
2017-03-12 00:20:20 +0100

    Testing something

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
+var AWS = require('aws-sdk');
+AWS.config.region = 'eu-west-1';
+var lambda = new AWS.Lambda();
 var request = require ( 'request' )
 
 console.log('Loading function');
