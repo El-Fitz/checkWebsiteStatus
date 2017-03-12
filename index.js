064af93ae454ebabf71d8262d17621a4a32bfbb0
Thomas Léger <El-Fitz@users.noreply.github.com>
2017-03-12 01:00:31 +0100

    Updated to remove the unnecessary extraction of the website's domain, subdomain and tld, and take into account the request's statusCode

--- index.js
+++ index.js
@@ -2,26 +2,21 @@
 var AWS = require('aws-sdk');
 AWS.config.region = 'eu-west-1';
 var lambda = new AWS.Lambda();
-var request = require ( 'request' )
+var request = require ( 'request' );
 
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
   let website = event.company.website;
-  extractWebsiteDomain(website, function(err, fullDomain) {
-    if (typeof fullDomain !== null && fullDomain !== null) {
-      checkStatus(fullDomain, function(err, data) {
-        if (err || addresses === null || typeof addresses == 'undefined' || addresses.length === 0) {
-          console.log("URL didn't resolve");
+      checkStatus(website, function(err, headers, statusCode) {
+
+        if (err || headers === null || typeof headers == 'undefined' || statusCode < 200 || (statusCode >= 400 && statusCode <= 499) || (statusCode >= 500 && statusCode <= 599)) {
+          console.log("Website doesn't work");
           event.websiteIsOnline = false;
         } else {
           event.websiteIsOnline = true;
         }
         context.done(null, event);
-      });
-    } else {
-      context.done(err);
-    }
   });
 };
 
@@ -34,28 +29,11 @@ function checkStatus(url, done) {
       "Content-Type": "application/json",
       "User-Agent": "ElFitz AWS Lambda/1.0"
     }
-  },
+  };
   request(options, function(err, data) {
     console.log("Headers: ", data.headers);
-    done(err, data);
-  }
-};
-
-function extractWebsiteDomain(website, done) {
-  var params = {
-    FunctionName: 'extractWebsiteDomainAndTLD',
-    InvocationType: 'RequestResponse',
-    LogType: 'Tail',
-    Payload: '{"website" : ' + JSON.stringify(website) + '}'
-  };
-
-  lambda.invoke(params, function(err, data) {
-    if (err) {
-      console.log("Err: ", err);
-      done(err, null);
-    } else {
-      let info = JSON.parse(data.Payload);
-      done(err, info.fullDomain);
-    }
+    console.log("Status Code: ", data.statusCode);
+    console.log("Error: ", err);
+    done(err, data.headers, data.statusCode);
   });
 }
